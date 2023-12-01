cbd86032fab1754d98fe8f0de201f65d0542d55f
Ethan <ethanytchang@gmail.com>
2023-12-01 16:37:19 -0600

    chakra ui getting started

---
+++ getting-started/pages/index.tsx
@@ -0,0 +1,18 @@
+import { Button, Flex, Heading, Input, useColorMode, useColorModeValue } from '@chakra-ui/react'
+
+const IndexPage = () => {
+  const { toggleColorMode } = useColorMode()
+  const formBackground = useColorModeValue('gray.100', 'gray.700')
+  return (
+    <Flex height='100vh' alignItems='center' justifyContent='center'>
+      <Flex direction='column' background={formBackground} p={12} rounded={6}>
+        <Heading mb={6}>Log in</Heading>
+          <Input placeholder='lazar@chakra-ui.com' variant='filled' mb={3} type='email'/>
+          <Input placeholder='******' variant='filled' mb={6} type='password'/>
+          <Button colorScheme='teal' mb={6}>Log in</Button>
+          <Button onClick={toggleColorMode}>Toggle Color Mode</Button>
+      </Flex>
+    </Flex>
+  )
+}
+export default IndexPage
